c4a30496315efcf6b312564d2eb72ba5badb55c6
smiley <consensusworld@protonmail.com>
2022-11-29 22:38:23 +0800

    evn url

--- src/components/buttons/sign.js
+++ src/components/buttons/sign.js
@@ -1,3 +1,4 @@
+const { signPageUrl } = process.env
 module.exports = {
     data: {
         name: `sign`
@@ -6,7 +7,7 @@ module.exports = {
         await interaction.reply({
             // content: `http://192.168.31.214:3000/?discordId=${interaction.user.id}&discordUserName=${interaction.user.tag}`
             // content: `http://172.29.112.1:3000/?discordId=${interaction.user.id}&discordUserName=${interaction.user.tag}`
-            content: `http://localhost:3000/?discordId=${interaction.user.id}&discordUserName=${interaction.user.tag}`
+            content: `${signPageUrl}/?discordId=${interaction.user.id}&discordUserName=${interaction.user.tag}`
             ,ephemeral: true
         })
     }
